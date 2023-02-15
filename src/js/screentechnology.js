c15332f4edecaf67a6f760c2f06fb3bd1e736fa6
Matheus Macedo <matheusg3.201168@gmail.com>
2023-02-14 22:22:56 -0300

    refactored technology page css, js, and css

--- src/js/screentechnology.js
+++ src/js/screentechnology.js
@@ -1,38 +1,49 @@
 const screenTechnology = {
     technologies: document.querySelector(".technologies"),
     renderTechnology(api) {
-        let abaTechnology = "";
+        let descriptionTechnology = "";
+        let imgTechnology = "";
 
         api.technology.forEach((technology, index) => {
-            abaTechnology += `<div class="technology" id="techno-${index}">
-                                <h2><span class="number">03</span> SPACE LAUNCH 101</h2>
-
-                                <div class="description">
-                                    <nav class="nav-techno">
-                                        <ol>
-                                            <li><a href="#" class="menu-techno selecionado" id="0">1</a></li>
-                                            <li><a href="#" class="menu-techno" id="1">2</a></li>
-                                            <li><a href="#" class="menu-techno" id="2">3</a></li>
-                                        </ol>
-                                    </nav>
-
-                                    <div class="info-tech">
-                                        <div class="info">
+            descriptionTechnology += `
+                                        <div class="info" id="info-${index}">
                                             <h3 class="sub-title">THE TERMINOLOGY…</h3>
                                             <h1 class="title">${technology.name}</h1>
                                             <p>${technology.description}</p>
                                         </div>
+                                     `;
 
-                                        <img class="img-tech" src="${technology.images.portrait}" alt="technology">
-                                    </div>
-                                </div>
-                              </div>
-                              `;
         });
 
-        console.log(abaTechnology);
+        api.technology.forEach((technology, index) => {
+            imgTechnology += `
+                                        <img class="img-tech" id="img-${index}" src="${technology.images.portrait}" alt="technology">
+                                     `;
+        });
 
-        this.technologies.innerHTML = `${abaTechnology}`;
+
+
+        this.technologies.innerHTML = `
+                                    <div class="technology">
+                                        <h2><span class="number">03</span> SPACE LAUNCH 101</h2>
+
+                                        <div class="description">
+                                            <nav class="nav-techno">
+                                                <ol>
+                                                    <li><a href="#" class="menu-techno selecionado" id="0">1</a></li>
+                                                    <li><a href="#" class="menu-techno" id="1">2</a></li>
+                                                    <li><a href="#" class="menu-techno" id="2">3</a></li>
+                                                </ol>
+                                            </nav>
+
+                                            <div class="info-tech">
+                                                ${descriptionTechnology}
+
+                                                ${imgTechnology}
+                                            </div>
+                                        </div>
+                                    </div>
+                                      `;
     },
 };
 
--- src/js/technology.js
+++ src/js/technology.js
@@ -1,38 +1,47 @@
-import { api } from "./data.js"
-import { screenTechnology } from "./screentechnology.js"
+import { api } from "./data.js";
+import { screenTechnology } from "./screentechnology.js";
 
-screenTechnology.renderTechnology(api)
+screenTechnology.renderTechnology(api);
 const menuTechno = document.querySelectorAll(".menu-techno");
-const technologySelecionada = document.getElementById("techno-0");
-technologySelecionada.classList.add("selecionado");
 
+mostrarClassSelecionado()
 
 menuTechno.forEach((techno) => {
   techno.addEventListener("click", () => {
+    adicionarOuRemoverClassDoMenu(techno);
 
-    adicionarOuRemoverClassDoMenu(techno)
-
-    mostrarTechnoSelecionadoNaTela(techno)
+    mostrarTechnoSelecionadoNaTela(techno);
   });
 });
 
-function adicionarOuRemoverClassDoMenu(techno){
-    const selecionado = document.querySelector(".menu-techno.selecionado");
-    selecionado.classList.remove("selecionado");
+function adicionarOuRemoverClassDoMenu(techno) {
+  const selecionado = document.querySelector(".menu-techno.selecionado");
+  selecionado.classList.remove("selecionado");
 
-    techno.classList.add("selecionado");
+  techno.classList.add("selecionado");
 }
 
-function mostrarTechnoSelecionadoNaTela(techno){
-    const technoComSelecionado = document.querySelector(".technology.selecionado");
+function mostrarTechnoSelecionadoNaTela(techno) {
+  const infoComSelecionado = document.querySelector(".info.selecionado");
+  infoComSelecionado.classList.remove("selecionado");
 
-    technoComSelecionado.classList.remove("selecionado");
+  const imgComSelecionado = document.querySelector(".img-tech.selecionado");
+  imgComSelecionado.classList.remove("selecionado");
 
-    const idDaTechnoParaSerMostrada = `techno-${techno.id}`;
+  const idDaImagem = `img-${techno.id}`;
 
-    const technoASerMostradoNaTela = document.getElementById(
-        idDaTechnoParaSerMostrada
-    );
+  const idDaInfo = `info-${techno.id}`;
 
-    technoASerMostradoNaTela.classList.add("selecionado");
+  const imgNaTela = document.getElementById(idDaImagem);
+  imgNaTela.classList.add("selecionado");
+
+  const infoNaTela = document.getElementById(idDaInfo);
+  infoNaTela.classList.add("selecionado");
+}
+
+function mostrarClassSelecionado() {
+  const infoSelecionada = document.getElementById("info-0");
+  const imgComSelecinado = document.getElementById("img-0");
+  imgComSelecinado.classList.add("selecionado");
+  infoSelecionada.classList.add("selecionado");
 }

0d046987daa817a1658003b5c1a498562245a1d9
Matheus Macedo <matheusg3.201168@gmail.com>
2023-03-07 22:01:15 -0300

    hamburger-menu effect

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,14 @@
 
+const center = document.querySelector('.center')
+center.addEventListener('click', ()=>{
 
-// const menuNavegation = document.querySelectorAll(".link")
+    center.classList.toggle('selecionado')
 
-// menuNavegation.forEach(nave => {
-//     nave.addEventListener('click', () =>{
-//         const selecionado = document.querySelector('.link.selecionado')
-//         selecionado.classList.remove('selecionado')
-
-//         nave.classList.add('selecionado')
-//     })
-// })
-
-const menuHamburguer = document.querySelector('.menu-hamburguer')
-
-menuHamburguer.addEventListener('click', () =>{
+    const iconeMenuHamburguer = document.querySelector('.center')
     
-    const iconeMenuHamburguer = document.querySelector('i')
+    const menuMob = document.querySelector('.menu-mob')
     
-    const menuComSelecionado = document.querySelector('.menu-mob')
-    
-    menuComSelecionado.classList.toggle('selecionado')
+    menuMob.classList.toggle('selecionado')
     if(menuComSelecionado){
         iconeMenuHamburguer.classList.toggle('fixed')
     }    
